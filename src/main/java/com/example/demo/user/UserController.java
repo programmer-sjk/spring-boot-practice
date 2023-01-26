fd71ecd62a85176801b0236ffcf0e6c3f29aacd9
sjk5766 <wjdrnr6662@gmail.com>
2023-01-26 19:14:25 +0900

    users 컨트롤러 추가

---
+++ src/main/java/com/example/demo/user/UserController.java
@@ -0,0 +1,15 @@
+package com.example.demo.user;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/users")
+public class UserController {
+    @GetMapping("/{userId}")
+    public String getUser(@PathVariable Long userId) {
+        return "get";
+    }
+}
